413577356863cf8c4f1ff44a87ab120106be3247
bradleymiller17 <36234270+bradleymiller17@users.noreply.github.com>
2018-04-23 11:22:35 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Bradley Miller 
+ * April 18 2018
+ * you will be able to play a game of hangman
+ * */
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace U3_BradleyMillerHUNGMAN
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void btn_PlayHangman_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
